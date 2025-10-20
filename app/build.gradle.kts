a77e994324efcca6a2a31435498b0122d782e216
Alex <megaxomyak@gmail.com>
2025-10-20 17:35:24 +0300

    modified build.gradle.kts removing dependencies for testing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,12 +13,6 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-}
-
-tasks.test {
-    useJUnitPlatform()
 }
 
 application {
