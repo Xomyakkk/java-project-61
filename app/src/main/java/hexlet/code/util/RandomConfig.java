b852236fa43ad183397dd0ccd2f34d17f861b960
Alex <megaxomyak@gmail.com>
2025-10-24 10:00:26 +0300

    style correct

--- app/src/main/java/hexlet/code/util/RandomConfig.java
+++ app/src/main/java/hexlet/code/util/RandomConfig.java
@@ -1,7 +1,7 @@
 package hexlet.code.util;
 
 public final class RandomConfig {
-    private RandomConfig () {}
+    private RandomConfig() { }
 
     public static final int MIN_NUMBER = 1;
     public static final int MAX_NUMBER = 100;
